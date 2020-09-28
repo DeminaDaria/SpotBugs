4146e35e96f08f4f2706b4ecfeea45f1a924f788
Demina Daria <demina.dasha92@gmail.com>
2020-09-29 00:13:31 +0300

    rename files

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,6 @@
 package ru.netology.statistic;
 
-public class StaticticsServise {
+public class StatisticsService {
     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
